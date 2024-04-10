e6bccdc6ec4649c5c76fef0a3dcb99b1364c16f3
Jeroen Jonker <jeroen.jonker@capteur.nl>
2024-04-10 11:27:05 +0200

    Moved clock to the top on small devices

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import BinaryClock from "@/components/BinaryClock.vue"
 </script>
 
 <template>
-  <div class="w-screen h-screen flex justify-center items-center">
+  <div class="w-screen h-screen flex justify-center md:items-center mt-10 md:mt-0">
     <BinaryClock />
   </div>
 </template>
